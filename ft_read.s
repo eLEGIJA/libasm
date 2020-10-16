a1569493653aee09c2c85cf7fc4042024786abf7
eLEGIJA <61831759+eLEGIJA@users.noreply.github.com>
2020-10-16 12:11:38 +0300

    write&read fix

--- ft_read.s
+++ ft_read.s
@@ -1,8 +1,20 @@
-    global	_ft_read
-	section	.text
+	segment	.text
+	global	_ft_read
 
 _ft_read:
 
-	mov		rax, 0x2000003
+	mov 	rax, 0x2000003
 	syscall
+
+	jc		_error
+	
+	jmp		_exit
+
+_error:
+
+	mov 	rax, -1
+	ret
+	
+_exit:
+
 	ret
--- ft_write.s
+++ ft_write.s
@@ -1,8 +1,23 @@
+	segment	.text
 	global	_ft_write
-	section	.text
 
 _ft_write:
 
-	mov	rax, 0x2000004
-	syscall
+	mov		r8, rdx
+	mov		ax, 0x2000004
+    syscall
+
+	jc		_error
+
+	jmp		_exit
+
+_error:
+
+	mov		rax, -1
 	ret
+
+_exit:
+
+	mov		rax, r8
+	ret
+	
